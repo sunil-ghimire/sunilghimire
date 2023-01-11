8b6a0f74ef1031194c16c7ada13179593afb0a0f
sunil-ghimire <pox420@gmail.com>
2023-01-12 01:20:47 +0545

    done with work

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,20 @@
 /*=============== Active Link =============== */
+const linkWork = document.querySelector('.category__btn');
+
+function activeWork() {
+    linkWork.forEach((a) => a.classList.remove('active-work'));
+    this.classList.add('active-work');
+}
+// linkWork.forEach((a) => a.addEventListener('click', activeWork));
 
 /*=============== Mixitup Filter =============== */
+let mixerProjects = mixitup('.projects__container', {
+    selectors: {
+        target: '.project__item'
+    },
+    animation: {
+        duration: 300,
+    }
+});
 
 /*=============== Testimonials Swiper =============== */
